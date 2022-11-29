1b0faff3a65de97b0379c341993bebb5c431d83e
Daniel Richerson <richerdani3@gmail.com>
2022-11-29 00:33:38 -0500

    Creando tabla

--- src/app/pages/promedio/promedio.component.ts
+++ src/app/pages/promedio/promedio.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-promedio',
   templateUrl: './promedio.component.html',
   styleUrls: ['./promedio.component.css']
 })
-export class PromedioComponent {
+export class PromedioComponent implements OnInit{
+
+  public items: any[] = [];
+  public promedio: boolean = false;
+
+  constructor() {}
+
+  ngOnInit(): void {
+    this.items = [];
+  }
+
+  submit() {
+    this.promedio = true;
+  }
 
 }
