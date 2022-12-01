22ef6541cca8ebc4500d8eb9a0380d24967011cd
Daniel Richerson <richerdani3@gmail.com>
2022-12-01 16:58:18 -0500

    Setteando el total

--- src/app/pages/promedio/promedio.component.ts
+++ src/app/pages/promedio/promedio.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { Materia } from '../../interfaces/materia.interface';
 
 @Component({
   selector: 'app-promedio',
@@ -8,29 +9,48 @@ import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 })
 export class PromedioComponent implements OnInit{
 
-  public items: any[] = [];
+  public materias: Materia[] = [];
   public promedio: boolean = false;
   public myForm!: FormGroup;
+  public total: number = 0;
   public add: boolean = false;
 
   constructor( private formBuilder: FormBuilder ) {}
 
   ngOnInit(): void {
     this.myForm = this.formBuilder.group({
-      materia: ['', [Validators.required ]],
-      nota: ['', [Validators.required ]],
-      numberOfCredits: ['', [Validators.required ]],
+      materia: ['', [ Validators.required ]],
+      nota: ['', [ Validators.required ]],
+      numberOfCredits: ['', [ Validators.required ]],
+      total: [ '',  [] ],
     });
   }
 
   addMateria() {
-    // this.add = true;
-    console.log(this.myForm.value);
-    this.myForm.reset();
+
+    if( !this.myForm.invalid ) {
+
+      this.add = true;
+      this.getTotal();
+      this.materias.push(this.myForm.value);
+      this.myForm.reset();
+
+    }
+  }
+
+  getTotal() {
+    this.total = 0;
+    const nota = this.myForm.get('nota')?.value;
+    const cantidadDeCreditos = this.myForm.get('numberOfCredits')?.value;
+    this.total = nota * cantidadDeCreditos;
+    this.myForm.get('total')?.setValue(this.total);
   }
 
   submit() {
-    this.promedio = true;
+    // this.promedio = true;
+    this.materias.forEach( data => {
+      console.log(data);
+    })
   }
 
 }
